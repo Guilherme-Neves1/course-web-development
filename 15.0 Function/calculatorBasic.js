dd898beeafd5ef17419589e63fcedce3a56610f6
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-01-11 13:36:40 -0300

    update

---
+++ 15.0 Function/calculatorBasic.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2); 
+}
+
+calculator(6, 3, div);
