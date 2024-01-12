3ded1ccfde4b87eceb33e3c6065988a1d0f29eac
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-01-12 16:09:24 -0300

    update

--- 20.0 Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ 20.0 Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -5,26 +5,56 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var started = false;
+
+var level = 0;
+
+$(document).keydown(function() {
+
+  if (!started) { // ! => OPERADOR NOT, started === false;
+    $("#level-title").html("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").on("click", function() {
   var userChosenColour = $(this).attr("id");
 
   userClickedPattern.push(userChosenColour);
 
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
+  level++;
+
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
+  
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-
+  playSound(randomChosenColour);
 }
 
-nextSequence();
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+
+
 
 
