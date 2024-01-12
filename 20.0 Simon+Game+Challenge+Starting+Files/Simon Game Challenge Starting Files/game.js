568f7e4732e3a4309762f0d74168c50ab1ef61ac
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-01-12 19:45:07 -0300

    update

--- 20.0 Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ 20.0 Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -2,17 +2,15 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
 var started = false;
-
 var level = 0;
 
 $(document).keydown(function() {
 
   if (!started) { // ! => OPERADOR NOT, started === false;
-    $("#level-title").html("Level " + level);
+    //$("#level-title").html("Level " + level);
     nextSequence();
     started = true;
   }
@@ -25,9 +23,42 @@ $(".btn").on("click", function() {
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    // console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length){
+
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+
+    playSound("wrong")
+
+    $("body").addClass("game-over");
+
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+
+    // console.log("wrong");
+
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
+
   level++;
 
   $("#level-title").text("Level " + level);
@@ -40,8 +71,13 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
-function playSound(name) {
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
 
+function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
