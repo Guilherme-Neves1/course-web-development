abf4137b73eec57abeb6368458172cd378da745a
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-01-16 20:35:28 -0300

    update

---
+++ 23.5 2.3 NPM/index.js
@@ -0,0 +1,25 @@
+// Versão CJS
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// Versão ESM
+// DICA: Usar 'import xxx from "sillyname"'
+// Assim após adicionar a biblioteca no from,
+// // o nome em import será sugerido.
+
+// import generateName from "sillyname";
+
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+
+const allNames = superheroes.all;
+
+const name = superheroes.random();
+
+console.log(`I am ${name}!`);
+
+
+
